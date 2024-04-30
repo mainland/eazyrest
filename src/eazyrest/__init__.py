0c0625760d63ddd99726b79358aa53e225b0c46c
Geoffrey Mainland <mainland@apeiron.net>
2024-04-30 12:51:56 -0400

    Initialize eazyrest __all__.

--- src/eazyrest/__init__.py
+++ src/eazyrest/__init__.py
@@ -1,2 +1,4 @@
 from .api import API
 from .eazyrest import DoesNotExist, MultipleObjectsReturned, JSONObject, json_object
+
+__all__ = ["API", "DoesNotExist", "MultipleObjectsReturned", "JSONObject", "json_object"]
