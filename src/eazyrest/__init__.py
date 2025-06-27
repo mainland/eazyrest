bfb45de1113bcacf5d906d40ad76bcf1fa599da4
Geoffrey Mainland <mainland@apeiron.net>
2025-06-27 13:32:38 -0400

    Clean up formatting.

--- src/eazyrest/__init__.py
+++ src/eazyrest/__init__.py
@@ -1,4 +1,10 @@
 from .api import API
 from .eazyrest import DoesNotExist, MultipleObjectsReturned, JSONObject, json_object
 
-__all__ = ["API", "DoesNotExist", "MultipleObjectsReturned", "JSONObject", "json_object"]
+__all__ = [
+    "API",
+    "DoesNotExist",
+    "MultipleObjectsReturned",
+    "JSONObject",
+    "json_object",
+]
